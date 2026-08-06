e3a77131d4c1e6a1df8830c90df398f3a2bfda40
Sagar Suwal <dangolsuwalsagar00@gmail.com>
2026-08-06 10:52:17 +0545

    CRUD method working witn localStorage

--- tier-1-foundations/01-devlinks/app.js
+++ tier-1-foundations/01-devlinks/app.js
@@ -1,4 +1,8 @@
 // ? to store the data we get from the user...
+const cardContainer = document.getElementById("links-container");
+const toastContainer = document.getElementById("toast");
+const toastMsg = document.getElementById("toast-msg");
+const NoOfLinks = document.getElementById("link-count");
 
 const links = loadLinks();
 renderLinks();
@@ -22,21 +26,51 @@ function saveLinks() {
 // ? getting the elements from HTML
 
 function renderLinks() {
+  countLinks();
   const emptyState = document.getElementById("empty-state");
 
   if (links.length === 0) {
     emptyState.classList.remove("hidden");
+    cardContainer.innerHTML = "";
+
     return;
   }
 
   emptyState.classList.add("hidden");
 
-  const cardContainer = document.getElementById("links-container");
-
   cardContainer.innerHTML = "";
-  links.forEach((curElem) => {
+  links.forEach((curElem, index) => {
     const infoContainer = document.createElement("div");
     infoContainer.classList.add("link-card__info");
+
+    const actionContainer = document.createElement("div");
+    actionContainer.classList.add("link-card__actions");
+
+    // * creating copy button
+    const copyBtn = document.createElement("button");
+    copyBtn.classList.add("copy-btn");
+    copyBtn.textContent = "copy";
+
+    // * adding event listener to the copyBtn
+
+    copyBtn.addEventListener("click", () => {
+      navigator.clipboard.writeText(curElem.url);
+
+      toastContainer.classList.remove("hidden");
+      toastMessage("successfully copied!");
+    });
+
+    // ? creating delete button
+    const dltBtn = document.createElement("button");
+    dltBtn.classList.add("dlt-btn");
+    dltBtn.textContent = "delte";
+    // ? adding event listener to the dlt button
+    dltBtn.addEventListener("click", () => {
+      links.splice(index, 1);
+      saveLinks();
+      renderLinks();
+    });
+
     const card = document.createElement("div");
     card.classList.add("link-card");
 
@@ -48,9 +82,12 @@ function renderLinks() {
     url.classList.add("link-card__url");
     url.textContent = curElem.url;
 
+    actionContainer.appendChild(copyBtn);
+    actionContainer.appendChild(dltBtn);
     infoContainer.appendChild(title);
     infoContainer.appendChild(url);
     card.appendChild(infoContainer);
+    card.appendChild(actionContainer);
     cardContainer.appendChild(card);
   });
 }
@@ -88,7 +125,24 @@ form.addEventListener("submit", (e) => {
   resetForm();
 });
 
-// ? reset the form after the user submits
+// * functiont to count the link
+
+function countLinks() {
+  const linkCount = links.length;
+  NoOfLinks.textContent = linkCount === 0 ? "0 links" : `${linkCount} links`;
+}
+
+// * function show toast message
+
+function toastMessage(msg) {
+  toastMsg.textContent = msg;
+
+  setTimeout(() => {
+    toastContainer.classList.add("hidden");
+  }, 2000);
+}
+
+// * reset the form after the user submits
 function resetForm() {
   form.reset();
 }
